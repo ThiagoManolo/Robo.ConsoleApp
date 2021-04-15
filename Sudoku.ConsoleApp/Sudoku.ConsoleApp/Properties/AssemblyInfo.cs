993f9ef8eaf507a0a7da5229ef74cd08741a2b75
ThiagoManolo <thiago0manolo@gmail.com>
2021-04-15 18:05:50 -0300

    Primeiro commit

--- Sudoku.ConsoleApp/Sudoku.ConsoleApp/Properties/AssemblyInfo.cs
+++ Sudoku.ConsoleApp/Sudoku.ConsoleApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("teste.ConsoleApp")]
+[assembly: AssemblyTitle("Sudoku.ConsoleApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("teste.ConsoleApp")]
+[assembly: AssemblyProduct("Sudoku.ConsoleApp")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ff044158-2037-4399-a08f-c5dee76ef524")]
+[assembly: Guid("2c7722fa-be10-4e23-ad81-b37b90b39a63")]
 
 // Version information for an assembly consists of the following four values:
 //
